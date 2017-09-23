c54926226ecc322674739d76153c9fa00f7b1ee2
dhuber666 <dhuber666@gmail.com>
2017-09-23 11:46:19 +0200

    Added Animation but it's not working

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
 
 $(document).ready(function() {
 
+
+    //TODO: When I click the SearchBtn the .search div should scroll on top of the page
+    $("#searchBtn").on("click", function(){
+        $(".container").animate({
+            "position" : "initial"
+
+        }, 1000);
+    });
+
+
+
 });
 
