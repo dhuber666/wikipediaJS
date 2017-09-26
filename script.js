193546df2026815210c367c9230bf5bf547ecb1d
dhuber666 <dhuber666@gmail.com>
2017-09-26 16:08:17 +0200

    Actual version - animation working but have to fix container flow

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 /* global variables */
 
+var searchInput = "";
 
 /* document ready */
 
 $(document).ready(function() {
 
-
     //TODO: When I click the SearchBtn the .search div should scroll on top of the page
-    $("#searchBtn").on("click", function(){
-        $(".container").animate({
-            "position" : "initial"
+    $("#searchBtn").on("click", function(){  // select the search button
+        $(".container").animate({ // select the .container
+            "top" : "0" //set it's position to initial
 
-        }, 1000);
+        }, 600, callback); // with 1 second transition
+        searchInput = $("#wikiSearch")[0].value;
+        
+        console.log(searchInput);
     });
-
-
-
 });
 
+function processResult(apiResult){
+    for (var i = 0; i < apiResult.query.search.length; i++){
+         $('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
+    }
+ }
+
+function callback(){ // gets called when sliding up the div is completed
+    $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        data: { action: 'query', list: 'search', srsearch: $("input[name=search]").val(), format: 'json' },
+        dataType: 'jsonp',
+        success: processResult
+    });
+}
+
+function search(ele) {
+    if(event.keyCode == 13) {
+        searchInput = ele.value; 
+    }
+    console.log(searchInput);
+}
+
+// Now  I have the search input saved into a variable. I can now make the api call
