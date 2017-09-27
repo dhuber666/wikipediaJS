34269bf3699f03af28ac6e502a8d5ddc3beba8ea
Huber Dominik <Huberdo@voestalpine.root.local>
2017-09-27 12:43:16 +0200

    fixed the document flow of document

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
     //TODO: When I click the SearchBtn the .search div should scroll on top of the page
     $("#searchBtn").on("click", function(){  // select the search button
         $(".container").animate({ // select the .container
-            "top" : "0" //set it's position to initial
+            "top" : "-200px" //set it's position to initial
 
-        }, 600, callback); // with 1 second transition
+        }, 300, callback); // with 1 second transition
         searchInput = $("#wikiSearch")[0].value;
         
         console.log(searchInput);
@@ -19,22 +19,30 @@ $(document).ready(function() {
 });
 
 function processResult(apiResult){
+    console.log(apiResult);
     for (var i = 0; i < apiResult.query.search.length; i++){
-         $('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
+         $('#display-result').append('<div class="' + i + '">' + '<p>'+apiResult.query.search[i].title+'</p>'  + '<p>'+ apiResult.query.search[i].snippet +'</p>' +  '</div>');
     }
  }
 
 function callback(){ // gets called when sliding up the div is completed
+    
     $.ajax({
         url: 'http://en.wikipedia.org/w/api.php',
-        data: { action: 'query', list: 'search', srsearch: $("input[name=search]").val(), format: 'json' },
+        //TODO: Fix this line of code (extracts)
+        data: { action: 'query', list: 'search', prop: 'extracts', exintro: true, srsearch: $("input[name=search]").val(), format: 'json' },
         dataType: 'jsonp',
         success: processResult
     });
+    $(".container").remove();
 }
 
 function search(ele) {
     if(event.keyCode == 13) {
+        $(".container").animate({ // select the .container
+            "top" : "0" //set it's position to initial
+
+        }, 300, callback); 
         searchInput = ele.value; 
     }
     console.log(searchInput);
